fb2f15554b9b815d2a4b96f949010739344e566c
RitaClose <104946160+RitaClose@users.noreply.github.com>
2023-08-08 10:39:22 +1200

    Updated Sketch.js
    A B and C letterforms using simple shapes: rounded rectangles and circles.

--- sketch.js
+++ sketch.js
@@ -13,28 +13,46 @@ const canvasHeight = 500;
  */
 
 const letterA = {
-  "size": 80,
-  "offsetx": 0,
-  "offsety": 35
+  "size1": 40,
+  "offsetx1": -5,
+  "offsety1": - 55,
+  "size2": 50,
+  "offsetx2": - 45,
+  "offsety2": - 15,
+  "size3": 70,
+  "offsetx3": - 90,
+  "offsety3": 45,
 }
 
 const letterB = {
-  "size": 150,
-  "offsetx": 0,
-  "offsety": -145
+  "size1": 40,
+  "offsetx1": - 35,
+  "offsety1": - 55,
+  "size2": 80,
+  "offsetx2": 45,
+  "offsety2": - 30,
+  "size3": 90,
+  "offsetx3": 45,
+  "offsety3": 30,
 }
 
 const letterC = {
-  "size": 100,
-  "offsetx": 30,
-  "offsety": 0
+  "size1": 40,
+  "offsetx1": 35,
+  "offsety1": 55,
+  "size2": 70,
+  "offsetx2": - 10,
+  "offsety2": 0,
+  "size3": 40,
+  "offsetx3": 35,
+  "offsety3": - 55,
 }
 
-const backgroundColor  = "#caf0f8";
-const strokeColor      = "#03045e";
+const backgroundColor  = "#fee0b0";
+const strokeColor      = "#ffffff";
 
-const darkBlue  = "#0077b6";
-const lightBlue  = "#90e0ef";
+const darkBlue  = "#faa9f0";
+const lightBlue  = "#faa9a9";
 
 function setup () {
   // create the drawing canvas, save the canvas element
@@ -65,15 +83,35 @@ function draw () {
 
 function drawLetter(posx, posy, letterData) {
   // determine parameters for second circle
-  let size2 = letterData["size"];
-  let pos2x = posx + letterData["offsetx"];
-  let pos2y = posy + letterData["offsety"];
+  let size1 = letterData["size1"];
+  let pos1x = posx + letterData["offsetx1"];
+  let pos1y = posy + letterData["offsety1"];
+  let size2 = letterData["size2"];
+  let pos2x = posx + letterData["offsetx2"];
+  let pos2y = posy + letterData["offsety2"];
+  let size3 = letterData["size3"];
+  let pos3x = posx + letterData["offsetx3"];
+  let pos3y = posy + letterData["offsety3"];
 
   // draw two circles
+  fill(255);
+  noStroke();
+  rectMode(CENTER);
+  rect(posx, posy, 30, 150, 5);
   fill(darkBlue);
-  ellipse(posx, posy, 150, 150);
+  ellipse(pos1x, pos1y, size1);
   fill(lightBlue);
-  ellipse(pos2x, pos2y, size2, size2);
+  ellipse(pos2x, pos2y, size2);
+  fill(darkBlue);
+  ellipse(pos3x, pos3y, size3);
+  noFill();
+  strokeWeight(2);
+  stroke(backgroundColor);
+  rect(posx - 10, posy + 31, 70, 10, 2);
+  circle(pos3x, pos3y, size3 - 10);
+  fill(255);
+  noStroke();
+  rect(posx - 10, posy + 30, 70, 10, 2);
 }
 
 function keyTyped() {
